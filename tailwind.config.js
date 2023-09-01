0d3a691a43ec0e1e8d3f4386a869d740839096ca
David Enikuomehin <enikuauth@gmail.com>
2023-09-01 07:07:35 +0100

    chore(*): import assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'work-sans': ['Work Sans', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        gray: {
+          100: '#F2F4F7',
+          200: '#EAECF0',
+          300: '#D0D5DD',
+          400: '#98A2B3',
+          500: '#667085',
+          600: '#475467',
+          700: '#344054',
+          800: '#1D2939',
+          900: '#101828',
+        },
+        primary: {
+          'off-white': '#F9F5FF',
+          blue: '#3F5BF6',
+          gray: {
+            100: '#EAECF0',
+            200: '#D0D5DD',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
