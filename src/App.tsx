994f78ca364e8f629d149e376e7f3f10a47073c4
David Enikuomehin <enikuauth@gmail.com>
2023-09-03 07:52:16 +0100

    feat(app): create intro header
    along with some custom css classes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,29 @@ const App = () => {
   return (
     <React.Fragment>
       <Navbar />
-      <div>{/* main */}</div>
+      <main className='padding-x'>
+        {/* Introduction Header */}
+
+        <div className='my-8 md:mt-12 sm:flex justify-between items-start'>
+          <div>
+            <h1 className='font-inter font-semibold text-2xl leading-8 text-gray-900 md:font-work-sans md:text-3xl md:leading-[2.375rem]'>
+              Good morning!
+            </h1>
+
+            <p className='text-alt-normal text-gray-600 mt-1'>
+              You got some task to do.
+            </p>
+          </div>
+
+          <button className='hidden sm:flex items-center work-sans-sm gap-2 px-4 py-[0.62rem] rounded-lg bg-primary-blue text-white shadow-[0_1px_2px_0px_rgba(16,24,40,0.05)]'>
+            <span>
+              <img src='/plus.svg' alt='' />
+            </span>
+
+            <span>Create New Task</span>
+          </button>
+        </div>
+      </main>
     </React.Fragment>
   );
 };
